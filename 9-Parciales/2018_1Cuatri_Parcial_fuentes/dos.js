9b3f46c718d5404adee646c4b565f4e86b876c89
Nicolas <40763535+Nickmanaos@users.noreply.github.com>
2018-07-04 12:01:44 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,9 @@
 function mostrar()
 {
-  
+  var nombre;
+  var localidad;
+
+  nombre=document.getElementById("nombre").value;
+  localidad=document.getElementById("localidad").value;
+  alert("usted es: " + nombre + "y vive en la localidad de " + localidad);
 }
